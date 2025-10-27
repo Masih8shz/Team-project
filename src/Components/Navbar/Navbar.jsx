b86a4c22049ae5df8a54abd9af5bb9e29c6d1ba9
Masih8shz <nightmre8shz4@gmail.com>
2025-10-27 22:10:34 +0330

    EditNavbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,22 @@
-import "../../Styles/Navbar.css"
-const Navbar = () => {
-    return(
-        <>
-        <div className="ContainerN">
+import "../../Styles/Navbar.css";
 
+import logoWeb from "../../Images/LogoWeb.png";
+const Navbar = () => {
+  return (
+    <>
+      <div className="ContainerN">
+        <img src={logoWeb} alt="Logo" className="LogoWebN"/>
+        <div>
+          <button className="BTN-links">Home</button>
+          <button className="BTN-links">Heroes</button>
+          <button className="BTN-links">Items</button>
+          <button className="BTN-links">Shop</button>
+          <button className="BTN-links">Tournaments</button>
+          <button className="BTN-links">Stats</button>
+          <button className="BTN-links">About</button>
         </div>
-        </>
-    )
-}
- export default Navbar;
+      </div>
+    </>
+  );
+};
+export default Navbar;
