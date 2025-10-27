9c7932fbaa1382d197dbed549e1f7a97e783b4d1
Masih8shz <nightmre8shz4@gmail.com>
2025-10-27 14:40:59 +0330

    Navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import "../../Styles/Navbar.css"
+const Navbar = () => {
+    return(
+        <>
+        <div className="ContainerN">
+
+        </div>
+        </>
+    )
+}
+ export default Navbar;
