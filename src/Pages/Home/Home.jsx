6d92dee9ba3c565d634abc0c5a7160c4c4b54dca
iman11111 <akfreefire1385@gmail.com>
2025-10-31 23:13:49 +0330

    shop

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,7 +5,8 @@ import heroes from "../../data/heroes.json";
 import items from "../../data/items.json";
 import { useEffect, useState } from "react";
 import BackgroundEffect from "../../Components/BackGroundEffect";
-
+import shop from "../../data/shop.json";
+import "../../Styles/shop.css";
 const Home = ({ isDark }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -35,6 +36,19 @@ const Home = ({ isDark }) => {
     visibleItems.push(...items.slice(0, 3 - visibleItems.length));
   }
   console.log("isDark:", isDark);
+  const [shopIndex, setShopIndex] = useState(0);
+
+useEffect(() => {
+  const i = setInterval(() => {
+    setShopIndex(prev => (prev + 2) % shop.length);
+  }, 4500);
+  return () => clearInterval(i);
+}, []);
+  
+const visibleShop = shop.slice(shopIndex, shopIndex + 2);
+if (visibleShop.length < 2) {
+  visibleShop.push(...shop.slice(0, 2 - visibleShop.length));
+}
   return (
     <div className="home-page">
       <BackgroundEffect isDark={isDark} />
@@ -103,7 +117,33 @@ const Home = ({ isDark }) => {
             ))}
           </div>
         </div>
+
+
+
+
+
       </section>
+      <section className="shop-section py-5">
+  <div className="container text-center">
+    <h2 className="section-title mb-5">Shop</h2>
+
+    <div className="row justify-content-center g-4">
+      {visibleShop.map((product, index) => (
+        <div key={index} className="col-md-5">
+          <div className="shop-card p-3">
+            <img src={product.img} alt={product.name} className="shop-img shop-img2 mb-3" />
+            <h4>{product.name}</h4>
+            <p className="shop-card2">{product.desc}</p>
+            <div className="d-flex justify-content-between align-items-center mt-3">
+              <strong className="text-warning">{product.price}</strong>
+              <button className="btn btn-success buy-btn">Buy</button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
+</section>
 
       <section className="py-5">
         <div className="container text-center">
