6a9d2efed84c45729c937923c541b68d06ec9805
iman11111 <akfreefire1385@gmail.com>
2025-11-02 10:44:10 +0330

    e

--- src/Pages/Items/Items.jsx
+++ src/Pages/Items/Items.jsx
@@ -3,17 +3,24 @@ import ItemCard from "../../Components/ItemCard/ItemCard";
 import "../../Styles/ItemsPage.css";
 import itemsData from "../../data/items.json";
 import BackgroundEffect from "../../Components/BackGroundEffect";
+
 const Items = ({ isDark }) => {
   const itemsPerPage = 30;
   const [currentPage, setCurrentPage] = useState(1);
 
   const totalPages = Math.ceil(itemsData.length / itemsPerPage);
+
+
   const startIndex = (currentPage - 1) * itemsPerPage;
+
   const currentItems = itemsData.slice(startIndex, startIndex + itemsPerPage);
-console.log("itemsData:", itemsData);
+
+  console.log("itemsData:", itemsData);
+
   return (
     <section className={`d2-items-section ${isDark ? "dark-mode" : ""}`}>
-       <BackgroundEffect isDark={isDark} />
+      <BackgroundEffect isDark={isDark} />
+
       <div className="d2-items-container">
         <h2 className="d2-items-title">Dota 2 Items</h2>
 
